498fcae4d88a650d50c95d0cd397a94d0e2ef509
GianDigia <digiacomogianmarco@gmail.com>
2020-05-01 16:26:05 +0200

    Add google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,12 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-165311276-1",
+        head: true,
+      },
+    },
   ],
 }
