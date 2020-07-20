4b1b3105c538d5ed0b127e553535a267d82cc7ed
GianDigia <digiacomogianmarco@gmail.com>
2020-07-20 22:21:16 +0200

    Build personalData component

--- src/components/personalData.js
+++ src/components/personalData.js
@@ -1,37 +1,42 @@
-import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
-import styled, { css } from 'styled-components'
-import { default as GImg } from 'gatsby-image'
-import { isBrowser } from 'react-device-detect'
+import styled from 'styled-components'
+import Section from './base/section'
+import Text from './base/text'
+import TooSerious from './images/tooSerious'
 
 const PersonalData = () => {
   return (
     <Wrapper>
-      <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+      <TooSerious />
+      <Info>
+        <Text>Gianmarco Digiacomo</Text>
+        <Text>digiacomogianmarco@gmail.com</Text>
+      </Info>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
-	margin-bottom: 1.45rem;
-	padding: 1.45rem 1rem 0 1rem;
+const Wrapper = styled(Section)` 
+  display: flex;
+  flex-direction: column;
+ 
+  @media only screen and (min-width: 426px) {
+    flex-direction: row;
+  }
+  
 `
 
-const Img = styled(GImg)`
-	border-radius: 100%;
-	max-width: 8rem;
-	margin: 0 auto;
-	display: block;
-	width: 8rem;
-
-	${isBrowser
-    ? css`
-				transition: transform 0.2s;
-				&:hover {
-					transform: scale(1.05);
-				}
-		  `
-    : null}
+const Info = styled.div`
+  display: flex;
+  flex-flow: column;
+  
+  ${Text} {
+    text-align: center;
+  }
+  
+  & > * {
+    margin: .5rem;
+  }
 `
 
 export default PersonalData
